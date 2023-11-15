2f4277081ecddb22e22b25136c84593aa033382e
João Pedro Bagnara Boeing <106355107+DevJoaoPBB@users.noreply.github.com>
2023-11-14 21:58:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,27 @@
 //LIGAÇÕES 
 let htmlPontuacao = document.getElementById("pontucao")
-let botaoNext = document.getElementById("btnNext")
 let botaoConcluir = document.getElementById("btnConcluir")
+
 
 const estadoSelect = document.getElementById('estado');
 const cidadeSelect = document.getElementById('cidade');
-
 estadoSelect.addEventListener('change', () => {
     const estadoSelecionado = estadoSelect.value;
-
-    // Limpa as opções atuais do dropdown de cidades
     cidadeSelect.innerHTML = '';
-
     if (estadoSelecionado !== "") {
-        // Cria uma opção para indicar as cidades do estado selecionado
         const estadoOption = document.createElement('option');
         estadoOption.disabled = true;
         estadoOption.selected = true;
         estadoOption.textContent = `Cidades do ${estadoSelecionado}`;
         cidadeSelect.appendChild(estadoOption);
-
-        // Faz a requisição à API do IBGE para obter as cidades do estado selecionado
         fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estadoSelecionado}/municipios`)
             .then(response => response.json())
             .then(data => {
-                // Preenche o dropdown de cidades com as opções retornadas pela API
                 data.forEach(cidade => {
                     const option = document.createElement('option');
-                    option.value = cidade.id; // Ou outra propriedade que identifique a cidade
+                    option.value = cidade.id; 
                     option.textContent = cidade.nome;
-                    cidadeSelect.appendChild(option); // Adiciona a opção de cidade ao dropdown
+                    cidadeSelect.appendChild(option); 
                 });
             })
             .catch(error => {
@@ -37,8 +29,6 @@ estadoSelect.addEventListener('change', () => {
             });
     }
 });
-
-
 
 //VARIAVEIS GLOBAIS
 
@@ -51,12 +41,20 @@ function pontuar() {
   htmlPontuacao.innerHTML = pontos
   console.log(tela)
   nextTela();
-}
 
-function nextTela() {
+}
+/* function nextTela() {
   if (tela ==  11) {
     botaoNext.style.display = 'none';
     botaoConcluir.style.display = 'block';
-    
   }
+} */
+
+function Adeus() {
+  console.log("Ação de conclusão realizada!");
+  document.querySelector('.quadrado-central').style.display = 'none';
 }
+
+botaoConcluir.addEventListener('click', function() {
+  Adeus();
+});
